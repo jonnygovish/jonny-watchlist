1e5ba96bec1e00517042a793e3ec408f7c98dd27
John mutavi <jonnygovish@gmail.com>
2018-07-17 18:26:03 +0300

    deployment to heroku

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    
+    #Initializing application
+    app = Flask(__name__)
+
+    # Setting up configuration
+    app.config.from_object(config_options[config_name])
+
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
+
+    return app
+
