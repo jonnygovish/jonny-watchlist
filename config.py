203fa25722cf0c13121482f8a8bdb4b143e14e8c
John mutavi <jonnygovish@gmail.com>
2018-07-27 10:41:59 +0300

    deploying to heroku again

--- config.py
+++ config.py
@@ -5,16 +5,33 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://puppah:zanpakutou@localhost/watchlist_test'
 
 class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://puppah:zanpakutou@localhost/watchlist'
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test': TestConfig
 }
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY=64e64c02cf7d5bb56d77fa7ae6c515cc
 export SECRET_KEY=I love me
+export MAIL_USERNAME='jonnymutavi@gmail.com'
+export MAIL_PASSWORD='Hakunapassword'
 
 python3.6 manage.py server
